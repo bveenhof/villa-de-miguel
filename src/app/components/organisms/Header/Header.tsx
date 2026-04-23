f74021a5bb4f4f21600a85ef60db5768c67f07f1
Bart <b.veenhof@gmail.com>
2026-04-23 17:12:11 +0200

    More styling and base setup

--- src/app/components/organisms/Header/Header.tsx
+++ src/app/components/organisms/Header/Header.tsx
@@ -1,31 +1,22 @@
 "use client"
 
-import { LinkType } from "@/app/types"
-import { Image, ImageProps } from "@/app/components";
+import { Image } from "@/app/components";
 import styles from './Header.module.scss';
-import Navigation from "../../molecules/Navigation/Navigation";
 
 export type headerProps = {
-    menuItems: Array<LinkType>;
-    heroImage?: ImageProps;
+    heroImage: string;
 }
-const Header = ({ menuItems }: headerProps) => {
+const Header = ({ heroImage }: headerProps) => {
 
     
     return (
         <header className={styles.header}>
-           {/* navigation  */}
-           {/* Hero banner with title and subtitle */}
-           {/* hero image */}
-
-           <Navigation menuItems={menuItems} />
-
-            <div className={styles["header__hero-content"]}>
-                <h1 className={styles["header__hero-title"]}>Welcome to Villa de Miguel</h1>
-                <p className={styles["header__hero-subtitle"]}>Experience the perfect blend of nature and comfort at our charming countryside retreat.</p>
+            <div className={styles['header__hero-content']}>
+                <h1 className={styles['header__hero-title']}>Welcome to Villa de Miguel</h1>
+                <p className={styles['header__hero-subtitle']}>Experience the perfect blend of nature and comfort at our charming countryside retreat.</p>
             </div>
 
-           <Image src="https://fastly.picsum.photos/id/671/1920/1080.jpg?hmac=CxdJymHXpEZxgxKDkJdt4ytqN-sa7xQL2j0ApTRzMo0" alt="Hero image" className={styles["header__hero-image"]} />
+           <Image src={heroImage} alt="Hero image" className={styles["header__hero-image"]} />
         </header>
     )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,38 +1,25 @@
 import Image from "next/image";
-import styles from "./page.module.css";
+import styles from "./page.module.scss";
 import { Button, Header } from "./components";
 
 export default function Home() {
 
-  const dummyMenuItems = [
-    { label: "About the villa", href: "#about-the-villa" },
-    { label: "Location", href: "#location" },
-    { label: "Rooms", href: "#rooms" },
-    { label: "Gallery", href: "#gallery" },
-    { label: "Contact", href: "#contact" },
-  ];
+  const dummyHeroImage = "https://fastly.picsum.photos/id/671/1920/1080.jpg?hmac=CxdJymHXpEZxgxKDkJdt4ytqN-sa7xQL2j0ApTRzMo0";
 
   return (
     <div className={styles.page}>
-      <Header menuItems={dummyMenuItems} />
+      <Header heroImage={dummyHeroImage} />
       <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className={styles.intro}>
-          <h1>Villa de Miguel</h1>
-          <p>
-            Text
-          </p>
+        <section className={styles.section}>
+          <div className={styles.container}>
+            <h1>Villa de Miguel</h1>
+            <p>
+              Text
+            </p>
 
-          <Button label="Button component" />
-        </div>
-
+            <Button label="Button component" />
+          </div>
+        </section>
       </main>
     </div>
   );
